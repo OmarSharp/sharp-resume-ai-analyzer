99ecd51e8bb60077fa345e2266bf6fd9199b8bd9
Omar Almasri <sharparray1994@gmail.com>
2025-10-25 14:26:47 +0300

    Create Homepage, Navbar & Resume Card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "sharp-resume-ai-analyzer" },
+    { name: "description", content: "Sharp Resume AI Analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+      <Navbar />
+
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Track Your App & Resume Rating</h1>
+              <h2>Review Your Submissions and check AI Powered Feedback</h2>
+          </div>
+
+
+      {resumes.length > 0 && (
+          <div className="resumes-section">
+              {resumes.map((resume) =>(
+                  <ResumeCard key={resume.id} resume={resume}/>
+              ))}
+
+          </div>
+      )}
+      </section>
+  </main>;
 }
